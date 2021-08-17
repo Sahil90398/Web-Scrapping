091d755b53459f24e995e4662e70573178d6debd
Sahil90398 <ranasahil90398@gmail.com>
2021-08-17 16:03:36 +0530

    first commit

---
+++ Class work/jsonReadWrite.js
@@ -0,0 +1,20 @@
+let fs = require("fs");
+// object ,array 
+// object -> contain string as key 
+// {
+//     "name": "Jasbir"
+// }
+ let input = ["hello", "hi", "how are you"];
+// write
+ //let jsonWriteAble = JSON.stringify(input);
+ //fs.writeFileSync("abc.json", jsonWriteAble);
+ //console.log("jsonwriteable",input)
+// read 
+let content = fs.readFileSync("abc.json");
+let jsonData = JSON.parse(content);
+console.log("content" + content)
+// append
+jsonData.push("Hola");
+let jsonWriteAble = JSON.stringify(jsonData);
+fs.writeFileSync("abc.json", jsonWriteAble);
+console.log("abc.json",jsonWriteAble)
